68babdfddf7f4f0204e98df0b707fbda93d3b0f8
Charles Peace <charlespeace@hotmail.com>
2022-05-06 18:33:11 -0700

    added thoughtmodel with thoughttext, createdat, username, and reactions + included moment.js for timestamp

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,35 @@
 const { Schema, model } = require('mongoose')
+const moment = require('moment')
+const reactionsSchema = require('./Reaction.js')
 
-const thoughtSchema = new Schema(
-    {
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
 
     },
-    {
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: moment().format('LLL')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionsSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+})
 
-    }
-)
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+})
 
 const Thought = model('Thought', thoughtSchema)
 
--- models/User.js
+++ models/User.js
@@ -1,37 +1,36 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            match: 
-            [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
-            'Please fill a valid email address']
-        },
-        thoughts: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }],
-        friends: [{
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }]
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-)
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: 
+        [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
+        'Please fill a valid email address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+})
 
 userSchema.virtual('friendCount').get(function() {
     return this.friends.length
