002f7ac7a0599ad975b8cd986d1bbbf3b5f07d17
Charles Peace <charlespeace@hotmail.com>
2022-05-23 15:45:43 -0700

    completed thought model and added reaction schema/model

--- models/Thought.js
+++ models/Thought.js
@@ -6,7 +6,8 @@ const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
-
+        minlength: 1,
+        maxlength: 280
     },
     createdAt: {
         type: Date,
